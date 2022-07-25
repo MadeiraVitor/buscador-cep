77528a153c0b843831b5d63a2c9cc0ab06c33752
MadeiraVitor <vitorsoutom@hotmail.com>
2022-07-25 11:12:21 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { FiSearch } from "react-icons/fi";
+import './App.css'
+import baseUrl from "./services/cep";
 
 function App() {
+
+  const [input, setInput] = useState('')
+  const [cep, setCep] = useState({})
+
+  async function handleSearch() {
+
+    if (input === '') {
+      alert('Para buscar digite um CEP')
+      return;
+    }
+
+    const response = await fetch(`${baseUrl}/${input}/json`)
+    return await response.json().then(function (data) {
+      setCep(data)
+      setInput("")
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="container">
+      <h1 className="title">Busque seu CEP</h1>
+
+      <div className="container-input">
+        <input type="text" placeholder="Digite seu CEP" value={input} onChange={(event) => setInput(event.target.value)} />
+
+        <button className="button-search" onClick={handleSearch}>
+          <FiSearch size={25} color="#000000" />
+        </button>
+      </div>
+
+      {Object.keys(cep).length > 0 && (
+        <div className="info">
+          <h2>CEP: {cep.cep}</h2>
+          <span>{cep.logradouro}</span>
+          <span>Complemento: {cep.complemento}</span>
+          <span>Bairro: {cep.bairro}</span>
+          <span>{cep.localidade} - {cep.uf}</span>
+        </div>
+      )}
+
+    </main>
   );
 }
 
---
+++ src/services/cep.js
@@ -0,0 +1,3 @@
+const baseUrl = 'https://viacep.com.br/ws'
+
+export default baseUrl
